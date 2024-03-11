f27373b07fdf710efe5e27846322a085f41cdc44
Erjoy Robles <elrobles1@up.edu.ph>
2024-03-11 10:43:58 +0800

    docs: added handout and samples for week 5

---
+++ Week 5/Sample 3.asm
@@ -0,0 +1,17 @@
+global _start
+section .data
+SYS_EXIT equ 60
+lpCnt dq 5
+sum dq 0
+section .text
+_start:
+mov rcx, qword[lpCnt] ; loop counter
+mov rax, 1 ; odd integer counter
+sum_loop:
+add qword[sum], rax ; sum current odd integer
+add rax, 2 ; set next odd integer
+loop sum_loop
+exit_here:
+mov rax, SYS_EXIT
+xor rdi, rdi
+syscall
